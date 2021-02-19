926d228b9c591270f44c4aa0ba8074410f039b04
Zhenya Uvarova <70705740+zhene4ka-tech@users.noreply.github.com>
2021-02-19 11:29:13 -0500

    added the image

---
+++ car.py
@@ -0,0 +1,9 @@
+class Car:
+    def __init__(self, color):
+        self.color = color
+
+    def get_color(self):
+        return self.color
+
+    def get_count_letters(self, word):
+        return len(word)
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import render_template
+from car import Car
+
 
 app = Flask(__name__)
 
@@ -11,12 +13,20 @@ def index():
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    car = Car('red')
+    lens = car.get_count_letters('hello my friend')
+    return render_template('about.html', color=car.get_color(), lens=lens)
 
 
 @app.route('/contacts')
 def contacts():
     return render_template('contacts.html')
+
+
+@app.route('/additional_info')
+def additional_info():
+    return render_template('additional_info.html')
+
 
 if __name__ == '__main__':
     app.run()
